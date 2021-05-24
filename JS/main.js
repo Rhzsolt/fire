b79d24945db4f461c118e51f3e20002d2905a58c
Programozas Karrier <info@programozaskarrier.hu>
2021-05-24 22:43:39 +0200

    eleje

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+document.getElementById('egy').onclick=function(){
+    document.getElementById('ker').innerHTML = 'Akkor jó '
+}
+document.getElementById('ket').onclick=function(){
+    document.getElementById('ker').innerHTML = 'Akkor oltsd el!!!! '
+}
